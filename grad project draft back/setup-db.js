d96d3e8937cb93aff0da3158f310f7f1c95d8d0a
yahia <ys5313944@gmail.com>
2026-04-26 23:36:51 +0300

    chore: include utility scripts, update VPS defaults in nuke-db and setup-db

--- grad project draft back/setup-db.js
+++ grad project draft back/setup-db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '80.65.211.27',
   database: process.env.DB_NAME || 'gradprojdb',
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: parseInt(process.env.DB_PORT || '3005', 10),
 });
 
 async function setup() {
